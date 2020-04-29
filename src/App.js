9d7a3c58895f0c3985c9252139d067d6a72eb2ff
Songwutt <5735512158@psu.ac.th>
2020-04-29 09:27:37 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-
 import Login from './components/Login';
 import auth from './firebase';
-
 import './App.css';
+
 
 function App() {
   const [session, setSession] = useState({
     isLoggedIn: false,
     currentUser: null,
     errorMessage: null
+    
+    
   });
+
 
   useEffect(() => {
     const handleAuth = auth.onAuthStateChanged(user => {
@@ -42,15 +43,15 @@ function App() {
     <div className="App">
       {session.isLoggedIn ? (
         <header className="App-header">
-                   <h1>welcome To {session.currentUser && session.currentUser.email}</h1>
+          <h1>Welcome To {session.currentUser && session.currentUser.email}</h1>
           <br></br>
           <button type="button" onClick={handleLogout}>
             Logout
           </button>
         </header>
       ) : (
-        <Login setSession={setSession} />
-      )}
+          <Login setSession={setSession} />
+        )}
     </div>
   );
 }
